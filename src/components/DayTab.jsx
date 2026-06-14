64732ca8c8e943e5c2d5e472c60a8c4a77bb7123
Kosteod <gameroomd@gmail.com>
2026-06-14 13:38:35 +0300

    fix v2.1

--- src/components/DayTab.jsx
+++ src/components/DayTab.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 export default function DayTab({date, settings, saveSettings}) {
   const [log,          setLog]         = useState(null);
   const [schedule,     setSchedule]    = useState([]);
